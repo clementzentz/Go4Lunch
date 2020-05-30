057eea0bc0732451d9a8daabdd7f63cd6343e0a6
Clement Zentz <clzt94@gmail.com>
2020-05-30 21:44:37 +0200

    refactor code, todo : bottom + drawer. start gmap fragment.

--- app/src/main/java/clement/zentz/go4lunch/BottomNavActivity.java
+++ app/src/main/java/clement/zentz/go4lunch/BottomNavActivity.java
@@ -7,18 +7,19 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import static clement.zentz.go4lunch.AuthActivity.RC_SIGN_IN;
+import java.util.Objects;
 
 public class BottomNavActivity extends AppCompatActivity {
 
@@ -27,15 +28,18 @@ public class BottomNavActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bottom_nav_activity);
 
-        BottomNavigationView navView = findViewById(R.id.bottom_nav_view);
+        /*configureNavDrawer();*/
+
+        BottomNavigationView bottomNavView = findViewById(R.id.bottom_nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_mapView, R.id.navigation_listRestaurant, R.id.navigation_workmates)
+                /*.setDrawerLayout(drawer)*/
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupWithNavController(bottomNavView, navController);
     }
 
     @Override
@@ -45,15 +49,43 @@ public class BottomNavActivity extends AppCompatActivity {
         return true;
     }
 
+/*    //configure nav drawer
+    private void configureNavDrawer(){
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        drawer.addDrawerListener(new ActionBarDrawerToggle(this, drawer, toolbar, R.string.app_name, R.string.app_name));
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("User Profile");
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.nav_drawer_lunch_item :
+                    startActivity(new Intent(getApplicationContext(), DetailRestaurant.class));
+                    //return true;
+                    break;
+                case R.id.nav_drawer_settings_item :
+                    //do something
+                    //return true;
+                    break;
+                case R.id.nav_drawer_logout_item :
+                    AuthUI.getInstance()
+                            .signOut(this)
+                            .addOnCompleteListener(task -> {
+                                startActivity(new Intent(this, AuthActivity.class));
+                            });
+                    //return true;
+                    break;
+            }
+            return false;
+        });
+    }*/
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        /*if (item.getItemId() == R.id.hamburger) {
-            Navigation.createNavigateOnClickListener(R.id.action_pop_out_of_game, null);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);*/
-
         if (item.getItemId() == R.id.nav_drawer_lunch_item) {
             return true;
         }if (item.getItemId() == R.id.nav_drawer_settings_item) {
